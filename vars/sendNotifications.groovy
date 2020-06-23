a1796fda61460cf00ebd743175cd53259d039b26
dengjianchao <dengjianchao@dwnews.com>
2020-06-23 09:42:46 +0000

    only slack.

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -27,12 +27,13 @@ def call(String buildStatus = 'STARTED') {
   // Send notifications
   slackSend (color: colorCode, message: summary)
 
-  hipchatSend (color: color, notify: true, message: summary)
-
+  // hipchatSend (color: color, notify: true, message: summary)
+  /*
   emailext (
       to: 'bitwiseman@bitwiseman.com',
       subject: subject,
       body: details,
       recipientProviders: [[$class: 'DevelopersRecipientProvider']]
     )
+  */
 }
